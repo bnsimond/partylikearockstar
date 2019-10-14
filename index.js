53d6ed2fde5cf14622327431e6845d9b65aed0bd
Barseana Simond <barseana@gmail.com>
2019-10-14 09:22:18 -0500

    97

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ let questions = [
   
    {
     title: "I cannot afford to have a party. What should I do?",
-    answers: ["Wait until you can afford to have a party.", "Do not have a party", "Have a Potluck", "Charge an Entry fee", "Play the lottery"],
-    correct: 0
+    answers: ["Have a party anyway", "Do not have a party", "Have a Potluck", "Charge an Entry fee", "Play the lottery"],
+    correct: 1
    },
   
    {
     title: "I am going to a birthday party for my cousin, what should I buy her?",
-    answers: ["Nothing", "Depends on her interests", "Regift what she bought you for your birthday", "Nothing more than $1.00", "A dog."],
+    answers: ["Nothing", "Depends on his/her interests", "Regift what they bought you for your birthday", "Nothing more than $1.00", "A dog."],
     correct: 1
    },
   
@@ -63,75 +63,60 @@ let questions = [
    },
   
    {
-    title: "How should I prepare for a party in home?",
-    answers: ["Clean bathroom", "Put the animals away", "Clean home", "Buy food", "Clean bathroom again"],
-    correct: 0
+    title: "How should I prepare for a party in my home?",
+    answers: ["Clean bathroom", "Put the animals away", "Clean home", "Buy food", "All of the above"],
+    correct: 4
    },
   ];
   
 //event listeners
 
 //starts quiz
-function startQuizGame(){
-    addEventListeners();
+function startQuizGame () {
+    addEventListeners () ;
 }
 
 //what happens when the start button is clicked
-function addEventListeners(){
-    $('.begin a, .again').click(function(e){
+function addEventListeners () { 
+    $('.begin a, .again').click(function(e) {
       //alert('clicked')
-      e.preventDefault();
+      e.preventDefault ()  ;
       currentQuestion = 0;
       totalQuestionsAnsweredCorrectly = 0;
-      //the start button hides and the quiz questions appear
-      $('.begin, .specifics, .more').hide();
-      $('.quiz, #submit').show();
-      $('.again').hide();
+      //these buttons hide and the quiz/submit and questions appear
+      $('.begin, .specifics, .more').hide () ;
+      $('.quiz, #submit').show () ;
+      $('.again').hide () ;
       //showQuestion function starts
-      showQuestion();
+      showQuestion () ;
     });
     
-   /* $("input:radio").click(function(e){
-      e.preventDefault();
-      if($('#id').length){
-        var guess = $('#id').attr('.id');
-        console.log(guess);
-     } else {
-        alert('You must select and answer to continue');
-      }
-    });*/
-
     //when the nextQuestion is clicked
-    $(".nextQuestion").click(function(e){
-        e.preventDefault();
-        showQuestion();
-        //specifics class and more class clears out or resets
-        $(".specifics, .more").text("")
-        //the submit button should reappear but it does not
-        //$("#submit").show();
-        $("#submit, .nextQuestion, .results").toggle();
-        document.getElementById("quizQuestions").reset();
-        })
-
-    //$(".nextQuestion").click(function(){
-    //    $("#submit, .nextQuestion, .results").toggle();
-    //    })
-         
-
+    $(".nextQuestion").click(function (e) {
+       e.preventDefault () ;
+       showQuestion () ;
+       //specifics class and more class clears out or resets
+       $(".specifics, .more").text("")
+       //the submit button should reappear but it does not
+       //$("#submit").show();
+       $("#submit, .nextQuestion, .results").toggle () ;
+       document.getElementById("quizQuestions").reset () ;
+       })
+      
     //when the submit button from the form is clicked
     $('#quizQuestions').submit(event=>{
-     event.preventDefault();
-         // alert('submitted')
-      $('.results, .specifics, .more').show();
-      let radioValue = $("input[name='Option']:checked").val();
-      //compares the selected value with the correct value
-      let correctAnswer = questions[currentQuestion].correct;
-     // score += 1; 
-      console.log("radio value = ", radioValue);
-      //if no option is selected -- Please choose an option appears.
+       event.preventDefault();
+       // alert('submitted')
+       $('.results, .specifics, .more').show();
+       let radioValue = $("input[name='Option']:checked").val();
+       //compares the selected value with the correct value
+       let correctAnswer = questions[currentQuestion].correct;
+       // score += 1; 
+       console.log("radio value = ", radioValue);
+       //if no option is selected -- Please choose an option appears.
       if (radioValue===undefined){
-        $(".specifics").text("Please choose an option.")
-        $(".more, .results").css("display", "none")
+       $(".specifics").text("Please choose an option.")
+       $(".more, .results").css("display", "none")
       } else {
         if (radioValue==correctAnswer){
           totalQuestionsAnsweredCorrectly ++
@@ -141,7 +126,7 @@ function addEventListeners(){
         }
         //if a value is selected, it shows what the correct answer is. 
         createSpecificText(radioValue, correctAnswer)
-        //submit button should not show and nextQuestion button should show. (is toggle needed?)
+        //hide submit button
         $("#submit").hide();
       
          currentQuestion++;
@@ -170,13 +155,6 @@ function addEventListeners(){
     }
     
   }
-  //pass into function
- // function checkAnswer () {
-   // let answer = questions[currentQuestion];
-    //if (correctAnswer === radioValue) {
-      //score++;
-    //}
-  //}
    
   function showSummary () {
     $('.quiz').hide();
@@ -195,35 +173,4 @@ function addEventListeners(){
     $(".specifics").html(tallyString+statString)
   }
 
-
- /* function getScore(){
-    score=0
-    var numQuestions=10;
-    for (var i=0;i<numQuestions;i++){
-    if (userInput[i]==answers[i]){
-    score += 1;
-    }
-    else{
-    score += 0;
-    }
-      
-    }
-    return score;
-  }*/
- 
-  //function totalScore(){
-  //("Your score is" +getScore()+"out of 10");
-  /*  
-  function restartQuiz(){
-    $('.more').hide();
-    $('.quiz').show();
-    newFunction();
-    currentQuestion=0;
-    showQuestion();
-
-    //function new() {
-    //  totalScore();
-   // }
-  }
-*/
 $(startQuizGame)
